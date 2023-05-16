235857b39fc5cbef410b3bf6b06e7dcfdbf3931b
koya <seiyo5555@gmail.com>
2023-05-16 15:07:49 +0900

    user

--- src/storage/psql_12/init.sh
+++ src/storage/psql_12/init.sh
@@ -1,15 +1,15 @@
 set -e
 
-VAL=`psql -d colpro -U user -t <<_EOF
-SELECT 1 FROM information_schema.tables WHERE table_name = 'certification_info';
+VAL=`psql -d elpro -U user -t <<_EOF
+SELECT 1 FROM information_schema.tables WHERE table_name = 'userinfo';
 _EOF`
 
 if [ $VAL = 1 ]; then
   echo "skip init db"
 else
   echo "start db init"
-  psql -d colpro -U user -f /docker-entrypoint-initdb.d/sql/certification_info_ddl.sql
-  psql -d colpro -U user -f /docker-entrypoint-initdb.d/sql/certification_info_dml.sql
+  psql -d elpro -U user -f /docker-entrypoint-initdb.d/sql/01_ddl.sql
+  psql -d elpro -U user -f /docker-entrypoint-initdb.d/sql/02_dml.sql
 fi
 
-echo "end db colpro init"
+echo "end db elpro init"
