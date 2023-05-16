406bb8197985f7ca4e8f5b17e481d90d6e979ac8
koya <seiyo5555@gmail.com>
2023-05-16 13:54:23 +0900

    user

--- src/storage/psql_12/sql/02_dml.sql
+++ src/storage/psql_12/sql/02_dml.sql
@@ -1373,7 +1373,8 @@ VALUES('2','60','TRUE','3',now());
 
 
 
-
+INSERT INTO public."certification_info"(certification_id,certification_name,expiration_date,issuing_authority,certification_level,certification_tag)
+VALUES('1','基本情報技術者試験','','IPA','2','IT');
 
 
 -- SEQUENCE
