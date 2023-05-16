86b34413067b99fed8b4f3642bed3d0c7e920871
koya <seiyo5555@gmail.com>
2023-05-16 17:14:41 +0900

    user

--- src/storage/psql_12/sql/certification_info_dml.sql
+++ src/storage/psql_12/sql/certification_info_dml.sql
@@ -15,4 +15,4 @@ VALUES('1','基本情報技術者試験','','IPA','2','IT');
 INSERT INTO public."certification_info"(certification_id,certification_name,expiration_date,issuing_authority,certification_level,certification_tag)
 VALUES('2','Java SE','','Oracle','2','IT');
 
-pg_catalog.setval('public."certification_info_certification_id_seq"', 2, true);
+SELECT pg_catalog.setval('public."certification_info_certification_id_seq"', 2, true);
