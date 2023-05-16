064b8b31784686ae9eaec6b0d7ad07f16069f68a
koya <seiyo5555@gmail.com>
2023-05-16 11:44:00 +0900

    koya

--- src/storage/psql_12/sql/01_ddl.sql
+++ src/storage/psql_12/sql/01_ddl.sql
@@ -196,16 +196,4 @@ ADD PRIMARY KEY(exam_event_id, question_id);
 --foreign key
 --primary key sequnence
 
-------certification_info table------------
-CREATE TABLE public."certification_info"(
-    certification_id  integer NOT NULL,
-    certification_name  character varying(50) NOT NULL,
-    expiration_date  integer,
-    issuing_authority  character varying(50) NOT NULL,
-    certification_level  character varying(1) NOT NULL,
-    certification_tag  character varying(10)
-);
-ALTER TABLE public."certification_info" OWNER TO "user";
---primary key
-ALTER TABLE ONLY public."certification_info"
-    ADD CONSTRAINT "certification_info_pkey" PRIMARY KEY (certification_id);
+
